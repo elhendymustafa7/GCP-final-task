1952ed4010682d9d023eefe7460f48f74c463bce
“elhendymustafa7” <elhendymustafa7@gmail.com>
2023-01-29 04:14:33 +0200

    readme:v1

--- terraform/image.tf
+++ terraform/image.tf
@@ -1,8 +1,5 @@
-resource "docker_registry_image" "test" {
-  name          = docker_image.image.name
-  keep_remotely = true
-}
 
+#build dockerfile
 resource "docker_image" "image" {
   name = "test"
   build {
